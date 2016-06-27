bc600ea7ef42ac3fe1a826363ab449fa7f09acb9
Vijayakrishna Naganoor <scivijay95@gmail.com>
2016-06-27 15:35:08 -0400

    Update Negatives_Extract.m

--- Data_Extraction/Negatives_Extract.m
+++ Data_Extraction/Negatives_Extract.m
@@ -1,19 +1,27 @@
-for i=1:3:length(M)
+for i=1:4:length(M)
     One = 'tme';
     Two = num2str(M(i,1));
     Three = '/';
     Four = 'Right/';
-    Five = '0';
-    Six = num2str(M(i,2));
+    Six = M(i,2);
+    if(Six>9999)
+        Five = '0';
+    elseif(Six>999)
+        Five = '00';
+    else
+        Five = '000';
+    end
+    Six = num2str(Six);
     Seven = '-R.png';
+    Eight = '-R.jpg';
     image = strcat(One,Two,Three,Four,Five,Six,Seven);
     image_name = strcat(Five,Six,Seven);
-    im = imread(image);
+    im= imread(image);
     Im = demosaic(im,'bggr');
     xmin = randi([1,700],1);
-    ymin = randi([1,900],1);
-    width = 32;
-    height = 32;
+    ymin = randi([1,700],1);
+    width = 63;
+    height = 63;
     image_segment = imcrop(Im,[xmin,ymin,width,height]);
     imwrite(image_segment,image_name)
     i
